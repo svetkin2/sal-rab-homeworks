ffe91650b9e512317ed43cd48754557b6b41a4f3
svetkin2@gmail.com <svetkin2@gmail.com>
2021-05-28 09:54:41 +0300

    task_4_2

--- task_4/solution.js
+++ task_4/solution.js
@@ -8,6 +8,9 @@ function parseProducts(json) {
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
    let data = JSON.parse(json);
+   let products = data.products;
+   
+   return products;
 }
 
 // Напишите функцию renderProductsCards(json)
@@ -17,7 +20,7 @@ function renderProductsCards(json){
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
     clearProducts();
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-    let products = parseProducts(json);
+    products = parseProducts(json);
 // Запишите в переменную length значение свойства products.length
     let length = products.length;
 // Напишите цикл, в котором перебираете все products от 0 до (length)
